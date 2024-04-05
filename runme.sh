87d561e7e1221caf1e32d53ff57a6974bd81b75f
wassim <wbvb41@gmail.com>
2024-04-05 08:52:54 +0100

    updated batch file

--- runme.sh
+++ runme.sh
@@ -1,3 +1,4 @@
-flex lexer.l
-bison -d synt.y
-gcc -o steemit synt.tab.c lex.yy.c 
+bison -d -o bison_out/synt.tab.c synt.y
+flex -o flex_out/lex.yy.c lexer.l
+gcc -o out/steemit bison_out/synt.tab.c flex_out/lex.yy.c 
+./out/steemit < examples/ex.txt
