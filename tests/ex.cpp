97c401b3efcbe432f8f98454c365b9d12628650a
wassim <wbvb41@gmail.com>
2024-04-13 20:59:11 +0100

    current valid test

--- tests/ex.cpp
+++ tests/ex.cpp
@@ -9,8 +9,39 @@ cv::Mat generateNegativePositiveInvertedImage(const cv::Mat& inputImage) {
   cv::imshow("Inverted Image", invertedImage);
   cv::Mat inputImage = cv::imread("input.jpg");
   ggg = cv::Mat::identifier();
+  ggg = 5;
   cv::Mat inputImage = cv::imread("input.jpg", cv::IMREAD_GRAYSCALE);
   cv::Mat invertedImage = generateNegativePositiveInvertedImage(inputImage);
+  if (ggg<sss && ggg>lll){
+    int x;
+    x = y;
+    cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+    cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+    cv::imshow("Original Image", inputImage);
+    if(ggg>= sss){
+      x = y;
+      cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+      cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+    }
+  }
+
+  for (int y = 0; y < inputImage.rows; ++y) {
+    for (int x = 0; x < inputImage.cols; ++x) {
+      x=5;
+    }
+  }
+
+  for (int y = 0; y < inputImage.rows; ++y){
+    x = y;
+    cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+    cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+    if(ggg>= sss){
+      x = y;
+      cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+      cv::Mat invertedImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+    }
+  }
+
 }
 
 int main(){
