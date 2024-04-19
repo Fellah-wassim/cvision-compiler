c218b4ba270aa91aea2b7fb712b298ff19f69617
wassim <wbvb41@gmail.com>
2024-04-19 15:53:00 +0100

    updated shell

--- runme.sh
+++ runme.sh
@@ -5,4 +5,4 @@ fi
 bison -d -o out/bison/synt.tab.c src/parser/parser.y
 flex -o out/flex/lex.yy.c src/lexer/lexer.l
 gcc -o out/cvision out/bison/synt.tab.c out/flex/lex.yy.c 
-./out/cvision < tests/ex.cpp
+./out/cvision < tests/function.cpp
