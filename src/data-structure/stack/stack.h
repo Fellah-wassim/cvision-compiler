45b57b0bbdfb35197df809f20fe0eb7060ddb1ed
wassim <wbvb41@gmail.com>
2024-05-23 02:19:55 +0100

    updated stack code

--- src/data-structure/stack/stack.h
+++ src/data-structure/stack/stack.h
@@ -1,5 +1,4 @@
-#ifndef STACK_H
-#define STACK_H
+#define MAX_SIZE 40
 
 typedef struct Stack
 {
@@ -7,12 +6,33 @@ typedef struct Stack
   int top;
 } Stack;
 
-Stack *create_stack();
+Stack *create_stack()
+{
+  Stack *stack = malloc(sizeof(Stack));
 
-void push(Stack *stack, int data);
+  stack->top = -1;
+  stack->items = malloc(sizeof(int) * MAX_SIZE);
 
-int pop(Stack *stack);
+  return stack;
+}
 
-int is_empty(Stack *stack);
+int is_empty(Stack *stack)
+{
+  return stack->top == -1;
+}
 
-#endif // STACK_H
+void push(Stack *stack, int item)
+{
+  if (stack->top == MAX_SIZE - 1)
+    return;
+
+  stack->items[++stack->top] = item;
+}
+
+int pop(Stack *stack)
+{
+  if (is_empty(stack))
+    return -1;
+
+  return stack->items[stack->top--];
+}
